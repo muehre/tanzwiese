ebfd12aed7b1fdbce082b40e046f2ae0510d7d9e
Alexander Mührenberg <muehre@MBP-Alex.local>
2023-06-17 10:02:15 +0200

    updated timetable

--- src/Database/Timetable.ts
+++ src/Database/Timetable.ts
@@ -282,8 +282,8 @@ const timetable = [
     },
     {
         "id": "106",
-        "startAt": "2023-06-17T11:00:00",
-        "endAt": "2023-06-17T14:00:00",
+        "startAt": "2023-06-17T13:00:00",
+        "endAt": "2023-06-17T15:00:00",
         "description": "Hochdruck! - funktioniert im Grunde wie jeder Stempel. Was dann auf Papierkarten, Taschen, T-Shirts, Tabaksbeutel... gedruckt werden soll liegt in unserer Hand.\n\nAls Druckplatten bring ich eine Sammlung an Linol-, Holz- und Gummiresten mit. In diese werden mit Hohlmessern nach Lust und, Laune die Motive geschnitten, um sie nachher mit einer Presse z.B. auf Karten, oder mit einem Löffel auf Textilien zu drucken. Das kann schwarzweiß oder (mehr-)farbig passieren und oft wiederholt werden. Sobald Motive vorhanden sind könnte man diese also austauschen und kombinieren.\n\nBringt gerne auch eigene Druckstöcke oder Werkzeug mit, falls ihr welche habt und dies vorher lest. Freu mich drauf!🎨🖼🧑‍🎨",
         "name": "Druckpresse",
         "openEnd": false,
